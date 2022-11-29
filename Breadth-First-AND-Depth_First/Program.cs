c765c58416fa613188232e8c3a7e7b6787f620cd
HaizhenHuang <hhuang56@ucmerced.edu>
2022-11-28 23:17:34 -0800

    completed

--- Breadth-First-AND-Depth_First/Program.cs
+++ Breadth-First-AND-Depth_First/Program.cs
@@ -28,6 +28,32 @@ tree.Add(80);
 // [70 20 40]
 // dequeue 70 AND enqueue 60 80
 // [20 40 60 80]
-// dequeue 20 40 60 80
+// dequeue 20 
+// [40 60 80]
+// dequeue 40
+// [60 80]
+// dequeue 60
+// [80]
+// dequeue 80
+// []
 
 dfs.DFSearch(tree);
+// []
+// push 50
+// [50]
+// pop 50 AND push 70 30
+// [30 70]
+// pop 30 AND push 40 20
+// [20 40 70]
+// pop 20
+// [40 70]
+// pop 40
+// [70]
+// pop 70 AND push 80 60
+// [60 80]
+// pop 60
+// [80]
+// pop 80
+// []
+
+
